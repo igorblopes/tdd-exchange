d600bd227ba074987f6ab8e1dd3c933e31053667
Igor Barros Lopes de Mello <igorblopes1993@gmail.com>
2020-10-18 11:06:18 -0300

    ensure throw when getCurrency throw

--- src/exchange/exchange.service.spec.ts
+++ src/exchange/exchange.service.spec.ts
@@ -54,6 +54,16 @@ describe('ExchangeService', () => {
       await expect(currenciesService.getCurrency).toBeCalledWith('USD');
       await expect(currenciesService.getCurrency).toHaveBeenLastCalledWith('BRL');
     });
+
+
+    it('should be throw when getCurrency throw', async () => {
+      (currenciesService.getCurrency as jest.Mock).mockRejectedValue(new Error);
+      await expect(
+        service.convertAmount({from: 'INVALID', to: 'BRL', amount: 1})
+        ).rejects.toThrow();
+    });
+
+    
     
   });
 });
